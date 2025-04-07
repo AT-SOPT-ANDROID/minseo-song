48244c01d59e31b17f645e0d8d404e2fdbb2dcd8
MinseoSONG <mins041600@naver.com>
2025-04-07 17:42:38 +0900

    [feat/#1] 공통 컴포넌트 - 티빙버튼 구현

---
+++ app/src/main/java/org/sopt/at/core/component/TvingButton.kt
@@ -0,0 +1,70 @@
+package org.sopt.at.core.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.sopt.at.R
+import org.sopt.at.core.util.noRippleClickable
+import org.sopt.at.presentation.ui.ui.theme.ATSOPTANDROIDTheme
+
+@Composable
+fun TvingButton(
+    label: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    isDisabled: Boolean = false,
+) {
+    val backgroundColor: Color
+    val textColor: Color
+
+    when (isDisabled) {
+        true -> {
+            backgroundColor = Color.DarkGray
+            textColor = Color.Gray
+        }
+
+        else -> {
+            backgroundColor = Color.Red
+            textColor = Color.White
+        }
+    }
+
+    Box(
+        modifier = modifier
+            .noRippleClickable(onClick)
+            .fillMaxWidth()
+            .background(color = backgroundColor, shape = RoundedCornerShape(4.dp))
+            .padding(vertical = 14.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = label,
+            color = textColor,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun PreviewTvingButton() {
+    ATSOPTANDROIDTheme {
+        TvingButton(
+            label = stringResource(R.string.button_sign_in),
+            onClick = {}
+        )
+    }
+}
--- app/src/main/java/org/sopt/at/presentation/ui/SignInActivity.kt
+++ app/src/main/java/org/sopt/at/presentation/ui/SignInActivity.kt
@@ -8,7 +8,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -16,7 +15,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
@@ -24,7 +22,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -41,6 +38,7 @@ import androidx.compose.ui.unit.sp
 import org.sopt.at.R
 import org.sopt.at.core.component.PasswordTextField
 import org.sopt.at.core.component.TvingBasicTextField
+import org.sopt.at.core.component.TvingButton
 import org.sopt.at.core.component.TvingTopBar
 import org.sopt.at.presentation.ui.ui.theme.ATSOPTANDROIDTheme
 import org.sopt.at.ui.theme.TvingTheme.colors
@@ -96,23 +94,12 @@ fun SignInScreen(
         )
         Spacer(Modifier.height(20.dp))
 
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(color = Color.DarkGray, shape = RoundedCornerShape(4.dp))
-                .padding(vertical = 14.dp)
-                .clickable {
-
-                },
-            contentAlignment = Alignment.Center
-        ) {
-            Text(
-                text = stringResource(R.string.button_sign_in),
-                color = Color.Gray,
-                fontSize = 16.sp,
-                fontWeight = FontWeight.Bold
-            )
-        }
+        TvingButton(
+            label = stringResource(R.string.button_sign_in),
+            onClick = {  },
+            isDisabled = id.isEmpty() || password.isEmpty(),
+            modifier = Modifier.fillMaxWidth()
+        )
         Spacer(Modifier.height(40.dp))
 
         Row(
