9c6eb9bfec621c54eb80c03faf6bc71ed9d517b1
MinseoSONG <mins041600@naver.com>
2025-04-24 22:39:18 +0900

    [feat/#3] MyScreen 구현

--- app/src/main/java/org/sopt/at/presentation/ui/my/MyViewModel.kt
+++ app/src/main/java/org/sopt/at/presentation/ui/my/MyViewModel.kt
@@ -1,5 +1,6 @@
 package org.sopt.at.presentation.ui.my
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -11,4 +12,12 @@ import javax.inject.Inject
 class MyViewModel @Inject constructor() : ViewModel() {
     private val _profileImage = MutableStateFlow<String>("https://image.tving.com/upload/profile/IG00000023_IMAGE_12.png/dims/resize/F_webp,400")
     val profileImage: StateFlow<String> = _profileImage.asStateFlow()
+
+    private val _id = MutableStateFlow<String>("")
+    val id: StateFlow<String> = _id.asStateFlow()
+
+    fun initId(context: Context) {
+        val prefs = context.getSharedPreferences("user_prefs", Context.MODE_PRIVATE)
+        _id.value = prefs.getString("id", "") ?: ""
+    }
 }
