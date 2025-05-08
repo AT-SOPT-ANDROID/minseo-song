805a74f324ce8f0c78861806455edda025f7eaa0
MinseoSONG <mins041600@naver.com>
2025-05-08 20:47:20 +0900

    [feat/#7] 마이페이지에 내 닉네임 조회 api 연동 구현

--- app/src/main/java/org/sopt/at/presentation/ui/my/MyViewModel.kt
+++ app/src/main/java/org/sopt/at/presentation/ui/my/MyViewModel.kt
@@ -1,24 +1,34 @@
 package org.sopt.at.presentation.ui.my
 
-import android.content.Context
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import org.sopt.at.domain.usecase.GetMyNickNameUseCase
 import javax.inject.Inject
 
 @HiltViewModel
-class MyViewModel @Inject constructor() : ViewModel() {
+class MyViewModel @Inject constructor(
+    private val getMyNickNameUseCase: GetMyNickNameUseCase
+) : ViewModel() {
     private val _profileImage =
         MutableStateFlow<String>("https://image.tving.com/upload/profile/IG00000023_IMAGE_12.png/dims/resize/F_webp,400")
     val profileImage: StateFlow<String> = _profileImage.asStateFlow()
 
-    private val _id = MutableStateFlow<String>("")
-    val id: StateFlow<String> = _id.asStateFlow()
+    private val _nickname = MutableStateFlow<String>("")
+    val nickname: StateFlow<String> = _nickname.asStateFlow()
 
-    fun initId(context: Context) {
-        val prefs = context.getSharedPreferences("user_prefs", Context.MODE_PRIVATE)
-        _id.value = prefs.getString("id", "") ?: ""
+    fun getMyNickName(
+        userId: Long
+    ) {
+        viewModelScope.launch {
+            getMyNickNameUseCase(userId)
+                .onSuccess { response ->
+                    _nickname.value = response.nickname
+                }
+        }
     }
 }
