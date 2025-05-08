f3cf8b0d1648949aacb6f69301eba7ed1745ad0d
MinseoSONG <mins041600@naver.com>
2025-05-08 19:58:05 +0900

    [feat/#7] AuthRepositoryImpl 구현

--- app/src/main/java/org/sopt/at/data/repositoryimpl/AuthRepositoryImpl.kt
+++ app/src/main/java/org/sopt/at/data/repositoryimpl/AuthRepositoryImpl.kt
@@ -3,8 +3,10 @@ package org.sopt.at.data.repositoryimpl
 import org.sopt.at.data.dataremote.datasource.AuthRemoteDataSource
 import org.sopt.at.data.mapper.todata.toData
 import org.sopt.at.data.mapper.todomain.toDomain
-import org.sopt.at.domain.model.SignUpInfo
-import org.sopt.at.domain.model.UserInfo
+import org.sopt.at.domain.model.SignInRequestInfo
+import org.sopt.at.domain.model.SignInResponseInfo
+import org.sopt.at.domain.model.SignUpRequestInfo
+import org.sopt.at.domain.model.SignUpResponseInfo
 import org.sopt.at.domain.repository.AuthRepository
 import javax.inject.Inject
 
@@ -12,9 +14,17 @@ import javax.inject.Inject
 class AuthRepositoryImpl @Inject constructor(
     private val authRemoteDataSource: AuthRemoteDataSource
 ) : AuthRepository {
-    override suspend fun signUp(signUpInfo: SignUpInfo): Result<UserInfo> =
+    override suspend fun signUp(signUpRequestInfo: SignUpRequestInfo): Result<SignUpResponseInfo> =
         runCatching {
-            val response = authRemoteDataSource.signUp(signUpInfo.toData())
+            val response = authRemoteDataSource.signUp(signUpRequestInfo.toData())
             response.data?.toDomain() ?: throw IllegalStateException("회원가입 응답이 null입니다.")
         }
+
+    override suspend fun signIn(signInInfo: SignInRequestInfo): Result<SignInResponseInfo> =
+        runCatching {
+            val response = authRemoteDataSource.signIn(signInInfo.toData())
+            response.data?.toDomain() ?: throw IllegalStateException("로그인 응답이 null입니다.")
+        }
+
+
 }
