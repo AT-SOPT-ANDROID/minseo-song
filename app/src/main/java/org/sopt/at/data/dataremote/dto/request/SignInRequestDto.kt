660c9c55bff35e61571be78224e2156fafbd6e61
MinseoSONG <mins041600@naver.com>
2025-05-08 19:56:03 +0900

    [feat] SignInDto 생성

---
+++ app/src/main/java/org/sopt/at/data/dataremote/dto/request/SignInRequestDto.kt
@@ -0,0 +1,10 @@
+package org.sopt.at.data.dataremote.dto.request
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class SignInRequestDto(
+    @SerialName("loginId") val loginId: String,
+    @SerialName("password") val password: String
+)
