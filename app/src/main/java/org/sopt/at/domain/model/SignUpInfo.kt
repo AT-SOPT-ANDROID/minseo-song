a89a8a9988f35b7dd091878825c4a4361b937acc
MinseoSONG <mins041600@naver.com>
2025-05-08 18:58:23 +0900

    [feat] Data class 구현

---
+++ app/src/main/java/org/sopt/at/domain/model/SignUpInfo.kt
@@ -0,0 +1,7 @@
+package org.sopt.at.domain.model
+
+data class SignUpInfo(
+    val loginId: String,
+    val password: String,
+    val nickname: String
+)
---
+++ app/src/main/java/org/sopt/at/domain/model/UserInfo.kt
@@ -0,0 +1,6 @@
+package org.sopt.at.domain.model
+
+data class UserInfo(
+    val userId: Int,
+    val nickname: String
+)
