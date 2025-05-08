92ce1722aa540550754282d01e00c47657ff5e11
MinseoSONG <mins041600@naver.com>
2025-05-08 20:38:13 +0900

    [feat/#7] 내 닉네임 조회 api 추가

--- app/src/main/java/org/sopt/at/data/dataremote/service/AuthService.kt
+++ app/src/main/java/org/sopt/at/data/dataremote/service/AuthService.kt
@@ -3,9 +3,12 @@ package org.sopt.at.data.dataremote.service
 import org.sopt.at.data.dataremote.dto.base.NullableBaseResponse
 import org.sopt.at.data.dataremote.dto.request.SignInRequestDto
 import org.sopt.at.data.dataremote.dto.request.SignUpRequestDto
+import org.sopt.at.data.dataremote.dto.response.GetMyNickNameResponseDto
 import org.sopt.at.data.dataremote.dto.response.SignInResponseDto
 import org.sopt.at.data.dataremote.dto.response.SignUpResponseDto
 import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface AuthService {
@@ -18,4 +21,9 @@ interface AuthService {
     suspend fun signIn(
         @Body signInRequestDto: SignInRequestDto
     ): NullableBaseResponse<SignInResponseDto>
+
+    @GET("/api/v1/users/me")
+    suspend fun getMyNickName(
+        @Header("userId") userId: Long
+    ):NullableBaseResponse<GetMyNickNameResponseDto>
 }
