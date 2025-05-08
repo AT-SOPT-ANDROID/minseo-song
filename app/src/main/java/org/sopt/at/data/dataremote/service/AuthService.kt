07d05b9dfa97be00d5c0426c99742dc606fa449e
MinseoSONG <mins041600@naver.com>
2025-05-08 19:56:15 +0900

    [feat] signIn api 추가

--- app/src/main/java/org/sopt/at/data/dataremote/service/AuthService.kt
+++ app/src/main/java/org/sopt/at/data/dataremote/service/AuthService.kt
@@ -1,7 +1,9 @@
 package org.sopt.at.data.dataremote.service
 
 import org.sopt.at.data.dataremote.dto.base.NullableBaseResponse
+import org.sopt.at.data.dataremote.dto.request.SignInRequestDto
 import org.sopt.at.data.dataremote.dto.request.SignUpRequestDto
+import org.sopt.at.data.dataremote.dto.response.SignInResponseDto
 import org.sopt.at.data.dataremote.dto.response.SignUpResponseDto
 import retrofit2.http.Body
 import retrofit2.http.POST
@@ -11,4 +13,9 @@ interface AuthService {
     suspend fun signUp(
         @Body signUpRequestDto: SignUpRequestDto
     ): NullableBaseResponse<SignUpResponseDto>
+
+    @POST("/api/v1/auth/signin")
+    suspend fun signIn(
+        @Body signInRequestDto: SignInRequestDto
+    ): NullableBaseResponse<SignInResponseDto>
 }
