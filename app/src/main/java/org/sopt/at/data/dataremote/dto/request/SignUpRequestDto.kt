7cd2a837af7fab92d9305f82bee9ab040e38c8ab
MinseoSONG <mins041600@naver.com>
2025-05-08 18:57:47 +0900

    [feat] SignUpDto 구현

---
+++ app/src/main/java/org/sopt/at/data/dataremote/dto/request/SignUpRequestDto.kt
@@ -0,0 +1,11 @@
+package org.sopt.at.data.dataremote.dto.request
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class SignUpRequestDto(
+    @SerialName("loginId") val loginId: String,
+    @SerialName("password") val password: String,
+    @SerialName("nickname") val nickname: String
+)
